ccd050ace62296820b84b28d7ba9d64082c66f43
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-07-24 20:48:13 -0400

    Header has selection box

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -8,6 +8,16 @@ it("renders without crashing", () => {
 
 it("renders logo", () => {
   const wrapper = shallow(<Header />);
-  const logo = <h1>langly</h1>;
+  const logo = <h1 className="header__h1">langly</h1>;
   expect(wrapper.contains(logo)).toEqual(true);
+});
+
+it("has selection box", () => {
+  const wrapper = shallow(<Header />);
+  const selector = (
+    <select>
+      <option value="English">English</option>
+    </select>
+  );
+  expect(wrapper.contains(selector)).toEqual(true);
 });
